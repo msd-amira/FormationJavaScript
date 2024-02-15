ecd8e7e62054efdc79f7cd2cd5daf968093d48d3
ELYES JRIDI <120260650+3LYESSSS@users.noreply.github.com>
2024-02-15 22:53:25 +0100

    Update codeIlyes.js

--- codeIlyes.js
+++ codeIlyes.js
@@ -6,3 +6,20 @@ function TestFunction(param1,param2,....)
 
 // Call TestFunction
 TestFunction(param1,param2,....);
+
+
+//***EXERCICE 4***
+function checkArray(arr) {
+  if (arr.length === 0) {
+      throw new Error("Array is empty.");
+  }
+  else
+  {
+      console.log("Array is not empty.");
+  }
+}
+
+const myArray = [1,3,5];
+checkArray(myArray);
+
+//***EXERCICE 3***
