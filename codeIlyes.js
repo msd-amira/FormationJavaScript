6c86e5caae4159ddbd24c69a36db5015cf0b6bab
ELYES JRIDI <120260650+3LYESSSS@users.noreply.github.com>
2024-02-16 01:27:16 +0100

    Update codeIlyes.js

--- codeIlyes.js
+++ codeIlyes.js
@@ -22,4 +22,31 @@ function checkArray(arr) {
 const myArray = [1,3,5];
 checkArray(myArray);
 
+//***EXERCICE 5***
+function split(String) {
+  return string.split('\n');
+}
+
+
+const string = `This is line 1.
+This is line 2.
+And this is line 3.`;
+
+const lines = split(string);
+console.log(lines);
+
 //***EXERCICE 3***
+function testInt(number) {
+  if (!Number.isInteger(number)) {
+    console.log("Number is not an integer.");
+  }
+  else
+  {
+      console.log(number + " is an integer.");
+  }
+}
+
+
+testInt(3);
+testInt(3.14);
+testInt("hello");
