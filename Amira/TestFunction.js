79b0a847a6b394827365e990b0c2568d43abb902
Amira Messaoud <amira.messaoud@concircle.com>
2024-02-18 13:42:51 +0100

    Add SumFunction

---
+++ Amira/TestFunction.js
@@ -0,0 +1,13 @@
+// Define a function named TestFunction that takes some parameters
+function SumFunction(a, b) {
+  console.log("SumFunction");
+
+  return a + b;
+}
+
+// Call TestFunction
+
+console.log(SumFunction(2, 5));
+
+
+//node Amira/TestFunction.js
